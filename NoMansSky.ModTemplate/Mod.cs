b92d37d0b509975859e69ecf2efb95cfc22268f4
ThatBomberDev <80880503+ThatBomberDev@users.noreply.github.com>
2022-07-05 11:22:54 +0500

    Update for new API Beta/ Reloaded II Update

--- NoMansSky.ModTemplate/Mod.cs
+++ NoMansSky.ModTemplate/Mod.cs
@@ -6,9 +6,8 @@ using libMBIN;
 using libMBIN.NMS.GameComponents;
 using libMBIN.NMS.Globals;
 using libMBIN.NMS.Toolkit;
-using System.Windows.Forms;
 using libMBIN.NMS;
-using System.Net.Http;
+
 
 namespace NoMansSky.ModTemplate
 {
@@ -18,8 +17,7 @@ namespace NoMansSky.ModTemplate
     public class Mod : NMSMod
     {
 
-        private static readonly HttpClient client = new HttpClient();
-
+        
         /// <summary>
         /// Initializes your mod along with some necessary info.
         /// </summary>
@@ -34,8 +32,8 @@ namespace NoMansSky.ModTemplate
             Game.OnProfileSelected += () => Logger.WriteLine("The player just selected a save file");
             Game.OnMainMenu += OnMainMenu;
             Game.OnGameJoined.AddListener(GameJoined);
-            CurrentSystem.OnPlanetLoaded += planetLoaded;
-            //CurrentSystem.OnSystemLoaded += systemChanges;
+            CurrentSystem.OnPlanetLoaded.AddListener(planet => planetLoaded(planet));
+            
 
 
 
@@ -55,18 +53,6 @@ namespace NoMansSky.ModTemplate
         public override void Update()
         {
 
-            
-
-           
-
-
-            // Here is an example of checking for keyboard keys
-            if (Keyboard.IsPressed(Key.UpArrow))
-            {
-
-                
-
-            }
 
             
 
@@ -74,7 +60,7 @@ namespace NoMansSky.ModTemplate
 
             if (Keyboard.IsPressed(Key.DownArrow))
             {
-                //testingNonGlobalFields();
+               
 
 
                 var inventory = Player.Exosuit.GetInventory();
@@ -103,10 +89,10 @@ namespace NoMansSky.ModTemplate
             {
                 
 
-                var mbinList = Game.MBinManager.GetAllMBIN();
+                var mbinList = Game.MBinManager.GetAllMBin();
                 foreach (var file in mbinList)
                 {
-                    var fileType = Game.MBinManager.GetMbinType(file.Name);
+                    var fileType = Game.MBinManager.GetMBinType(file.Name);
                     Logger.WriteLine($"Struct [{file.Name}] has an address of {file.Address}, and is of type: {fileType}");
                     
                     
@@ -119,321 +105,18 @@ namespace NoMansSky.ModTemplate
 
         }
 
-       private void systemChanges(long systemAddress)
+
+        private void planetLoaded(IPlanet planet)
         {
-            Logger.WriteLine("Aquired System Address");
-
-            var system = CurrentSystem.GetSystemData();
-            system.Planets = 8;
-
-            system.Name.Value = "Abyssal System: C0??uP7Ed";
-
-            system.Light.SunColour.R =  Random.Range(0.000f, 1.000f);
-            system.Light.SunColour.G =  Random.Range(0.000f, 1.000f);
-            system.Light.SunColour.B =  Random.Range(0.000f, 1.000f);
-            system.Light.SunColour.A =  Random.Range(0.000f, 1.000f);
-
-            system.Light.LightColour.R =  Random.Range(0.000f, 1.000f);
-            system.Light.LightColour.G =  Random.Range(0.000f, 1.000f);
-            system.Light.LightColour.B =  Random.Range(0.000f, 1.000f);
-            system.Light.LightColour.A =  Random.Range(0.000f, 1.000f);
-
-            var systemColours = system.Colours.Palettes;
-            foreach (var pallete in systemColours)
+            planet.ModifyPlanetData(planetData =>
             {
-                for (var i = 0; i < pallete.Colours.Length; i++)
-                {
-                    Colour testColour = new Colour();
-                    testColour.A =  Random.Range(0.000f, 1.000f);
-                    testColour.R =  Random.Range(0.000f, 1.000f);
-                    testColour.G =  Random.Range(0.000f, 1.000f);
-                    testColour.B =  Random.Range(0.000f, 1.000f);
-                    pallete.Colours[i] = testColour;
-
-
-                }
-
+                planetData.SentinelData.MaxActiveDrones = 100;
+                planetData.Terrain.MinimumCaveDepth = 500;
 
             }
-
-            foreach (var planetPos in system.PlanetPositions)
-            {
-                var posRandomizer = new Vector3f();
-                posRandomizer.x =  Random.Range(0, 8388608);
-                posRandomizer.y =  Random.Range(0, 524288);
-                posRandomizer.z =  Random.Range(0, 8388608);
-                planetPos.x = posRandomizer.x;
-                planetPos.y = posRandomizer.y;
-                planetPos.z = posRandomizer.z;
-
-
-            }
-
-
-        }
-
-        private void planetLoaded(long planetAddress)
-        {
+            );
             
-            
-
-
-            var planet = CurrentSystem.GetPlanetData(planetAddress);
-            Logger.WriteLine($"Planet Loaded: {planet.Name.Value.ToString()}");
-
-
-
-            for (var i = 0; i < planet.TileColours.Length; i++)
-            {
-                Colour testColour = new Colour();
-                testColour.A =  Random.Range(0.000f, 1.000f);
-                testColour.R =  Random.Range(0.000f, 1.000f);
-                testColour.G =  Random.Range(0.000f, 1.000f);
-                testColour.B =  Random.Range(0.000f, 1.000f);
-
-                planet.TileColours[i] = testColour;
-
-
-            }
-
-
-            var alienChance =  Random.Range(0, 7);
-
-            if (alienChance == 0)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Traders;
-            }
-            else if (alienChance == 1)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Warriors;
-            }
-            else if (alienChance == 2)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Explorers;
-            }
-            else if (alienChance == 3)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Robots;
-            }
-            else if (alienChance == 4)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Atlas;
-            }
-            else if (alienChance == 5)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Diplomats;
-            }
-            else if (alienChance == 6)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.Exotics;
-            }
-            else if (alienChance == 7)
-            {
-                planet.InhabitingRace.AlienRace = GcAlienRace.AlienRaceEnum.None;
-            }
-
-
-
-
-
-            /*
-            planet.CommonSubstanceID.Value = "AF_METAL";
-            planet.UncommonSubstanceID.Value = "QUICKSILVER";
-            planet.RareSubstanceID.Value = "TECHFRAG";
-            */
-
-            var atmosChance =  Random.Range(0, 99);
-            if (atmosChance < 29)
-            {
-                planet.Weather.AtmosphereType = GcPlanetWeatherData.AtmosphereTypeEnum.None;
-            }
-            else if(atmosChance > 29)
-            {
-                planet.Weather.AtmosphereType = GcPlanetWeatherData.AtmosphereTypeEnum.Normal;
-            }
-
-            var stormFreqChance =  Random.Range(0, 2);
-            if (stormFreqChance ==0)
-            {
-                planet.Weather.StormFrequency = GcPlanetWeatherData.StormFrequencyEnum.None;
-            }
-            else if(stormFreqChance == 1)
-            {
-                planet.Weather.StormFrequency = GcPlanetWeatherData.StormFrequencyEnum.Low;
-            }
-            else if (stormFreqChance == 2)
-            {
-                planet.Weather.StormFrequency = GcPlanetWeatherData.StormFrequencyEnum.High;
-            }
-
-            var weatherIntensityChance =  Random.Range(0, 99);
-            if(weatherIntensityChance > 19)
-            {
-                planet.Weather.WeatherIntensity = GcPlanetWeatherData.WeatherIntensityEnum.Default;
-            }
-            else if(weatherIntensityChance < 19)
-            {
-                planet.Weather.WeatherIntensity = GcPlanetWeatherData.WeatherIntensityEnum.Extreme;
-            }
-
-            var lifeChance =  Random.Range(0, 3);
-            if(lifeChance ==0)
-            {
-                planet.Life.LifeSetting = GcPlanetLife.LifeSettingEnum.Dead;
-            }
-            else if(lifeChance == 1)
-            {
-                planet.Life.LifeSetting = GcPlanetLife.LifeSettingEnum.Low;
-            }
-            else if(lifeChance == 2)
-            {
-                planet.Life.LifeSetting = GcPlanetLife.LifeSettingEnum.Mid;
-            }
-            else if(lifeChance == 3)
-            {
-                planet.Life.LifeSetting = GcPlanetLife.LifeSettingEnum.Full;
-            }
-
-            var creatureChance =  Random.Range(0, 3);
-            if(creatureChance ==0)
-            {
-                planet.CreatureLife.LifeSetting = GcPlanetLife.LifeSettingEnum.Dead;
-            }
-            else if(creatureChance == 1)
-            {
-                planet.CreatureLife.LifeSetting = GcPlanetLife.LifeSettingEnum.Low;
-            }
-            else if( creatureChance == 2)
-            {
-                planet.CreatureLife.LifeSetting = GcPlanetLife.LifeSettingEnum.Mid;
-            }
-            else if(creatureChance == 3)
-            {
-                planet.CreatureLife.LifeSetting = GcPlanetLife.LifeSettingEnum.Full;
-            }
-
-            var primeChance =  Random.Range(0, 1);
-            if(primeChance == 0)
-            {
-                planet.GenerationData.Prime = false;
-            }
-            else if(primeChance == 1)
-            {
-                planet.GenerationData.Prime = true;
-            }
-
-            var sizeChance =  Random.Range(0, 3);
-            if (sizeChance == 0)
-            {
-                planet.GenerationData.Size.PlanetSize = GcPlanetSize.PlanetSizeEnum.Large;
-            }
-            else if(sizeChance == 1)
-            {
-                planet.GenerationData.Size.PlanetSize = GcPlanetSize.PlanetSizeEnum.Medium;
-            }
-            else if (sizeChance ==2)
-            {
-                planet.GenerationData.Size.PlanetSize = GcPlanetSize.PlanetSizeEnum.Small;
-            }
-            else if (sizeChance == 3)
-            {
-                planet.GenerationData.Size.PlanetSize = GcPlanetSize.PlanetSizeEnum.Moon;
-            }
-            
-            
-            planet.Weather.WeatherType.Weather = GcWeatherOptions.WeatherEnum.Humid;
-            planet.Weather.StormScreenFilter.ScreenFilter = GcScreenFilters.ScreenFilterEnum.FreighterAbandoned;
-            planet.Weather.ScreenFilter.ScreenFilter = GcScreenFilters.ScreenFilterEnum.NewVintageBright;
-            //planet.Weather.HeavyAir.Filename = "";
-
-            
-
-            
-            var aeronChance =  Random.Range(0, 2);
-            if (aeronChance == 0)
-            {
-                planet.SentinelData.SentinelLevel = GcPlanetSentinelData.SentinelLevelEnum.Low;
-                planet.SentinelData.MaxActiveDrones =  Random.Range(1, 5);
-            }
-            else if (aeronChance == 1)
-            {
-                planet.SentinelData.SentinelLevel = GcPlanetSentinelData.SentinelLevelEnum.Default;
-                planet.SentinelData.MaxActiveDrones =  Random.Range(6, 11);
-
-            }
-            else if (aeronChance == 2)
-            {
-                planet.SentinelData.SentinelLevel = GcPlanetSentinelData.SentinelLevelEnum.Aggressive;
-                planet.SentinelData.MaxActiveDrones =  Random.Range(12, 17);
-
-            }
-            
-            planet.FlybyTimer.x = 600;
-            planet.FlybyTimer.y = 600;
-
-            /*
-            planet.Clouds.Seed.Seed = ((long) Random.Range(0, 9223372036854775807));
-            Logger.WriteLine($"Cloud Seed: {planet.Clouds.Seed.Seed}");
-            planet.Clouds.Seed.UseSeedValue = true;
-            */
-            
-
-            
-            /*
-            var seedRandomizer = new GcSeed();
-            seedRandomizer.Seed = ((long) Random.Range(0, 9223372036854775807));
-            seedRandomizer.UseSeedValue = true;
-            planet.GenerationData.Seed = seedRandomizer;
-            */
-            
-
-            foreach(var index in planet.TileTypeIndices)
-            {
-                planet.TileTypeSet =  Random.Range(0, index);
-            }
-
-            foreach(var feature in planet.Terrain.Features)
-            {
-                feature.Active = true;
-
-
-            }
-
-            planet.Terrain.MinimumCaveDepth = 500;
-
-            
-
-            foreach(var creature in planet.SpawnData.Creatures)
-            {
-                creature.AllowFur = true;
-                creature.MaxScale = 10f;
-                creature.MinScale = 0.5f;
-                creature.CreatureMaxGroupSize = 50;
-                creature.CreatureMinGroupSize = 20;
-                creature.Herd = true;
-            }
-
-            planet.GenerationData.CreatureRoles.HasSandWorms = true;
-            
-
-            var planetColours = planet.Colours.Palettes;
-            foreach (var pallete in planetColours)
-            {
-                for (var i = 0; i < pallete.Colours.Length; i++)
-                {
-                    Colour testColour = new Colour();
-                    testColour.A =  Random.Range(0.000f, 1.000f);
-                    testColour.R =  Random.Range(0.000f, 1.000f);
-                    testColour.G =  Random.Range(0.000f, 1.000f);
-                    testColour.B =  Random.Range(0.000f, 1.000f);
-                    pallete.Colours[i] = testColour;
-
-
-                }
-
-
-            }
-            CurrentSystem.SetPlanetData(planetAddress, planet);
+          
         }
 
         private void structTest()
@@ -441,7 +124,7 @@ namespace NoMansSky.ModTemplate
             
 
             var memMgr = new MemoryManager();
-            var missionTableAddress = Game.MBinManager.GetMbin("GcMissionTable").Address;
+            var missionTableAddress = Game.MBinManager.GetMBin("GcMissionTable").Address;
             var missionTableInMem = memMgr.GetValue<GcMissionTable>(missionTableAddress);
             Logger.WriteLine($"The Amount Of Missions Loaded Is: {missionTableInMem.Missions.Count}");
             for(var i=0; i< missionTableInMem.Missions.Count; i++)
@@ -451,7 +134,7 @@ namespace NoMansSky.ModTemplate
 
             }
 
-            var combatEffectsTableAddress = Game.MBinManager.GetMbin("GcCombatEffectsTable").Address;
+            var combatEffectsTableAddress = Game.MBinManager.GetMBin("GcCombatEffectsTable").Address;
             var GcCombatEffectsTableInMem = memMgr.GetValue<GcCombatEffectsTable>(combatEffectsTableAddress);
             Logger.WriteLine($"Currently Loaded {GcCombatEffectsTableInMem.EffectsData.Length} effects");
             for(var i=0; i<GcCombatEffectsTableInMem.EffectsData.Length; i++)
